d2324072f12e81d5330c130d4afc3bdcad2ab197
Ivo Nunes <inunes1904@gmail.com>
2024-12-01 17:55:45 +0000

    Comunicating with Remote Server

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,7 +13,10 @@ const Login = ({ onLogin }) => {
     setSuccess('');
 
     try {
-      const response = await axios.post('http://localhost:3000/api/v1/login', {
+      // LOCAL
+      // const response = await axios.post('http://localhost:3000/api/v1/login', {
+      // REMOTE SERVER
+      const response = await axios.post('https://sommelierpath-2.onrender.com/api/v1/login', {
         email,
         password,
       });
