83d24b7433f78dff8e091b91a2e56e089e363988
Ivo Nunes <inunes1904@gmail.com>
2024-12-07 22:40:29 +0000

    Alerts Done ready to deploy colors

--- src/components/CustomAlert.js
+++ src/components/CustomAlert.js
@@ -2,34 +2,34 @@ import React from "react";
 import { Snackbar, Alert as MuiAlert } from "@mui/material";
 
 const CustomAlert = ({ open, message, type, onClose }) => {
-  // Map severity types to RGBA colors with reduced opacity
+
   const backgroundColors = {
-    success: "rgba(76, 175, 80, 0.5)", // Green with opacity
-    error: "rgba(244, 67, 54, 0.5)", // Red with opacity
-    warning: "rgba(255, 152, 0, 0.5)", // Orange with opacity
-    info: "rgba(33, 150, 243, 0.5)", // Blue with opacity
+    success: "rgba(76, 175, 80, 0.5)",
+    error: "rgba(244, 67, 54, 0.5)",
+    warning: "rgba(255, 152, 0, 0.5)",
+    info: "rgba(33, 150, 243, 0.5)",
   };
 
   return (
     <Snackbar
       open={open}
-      autoHideDuration={3000} // Automatically hide after 3 seconds
+      autoHideDuration={3000}
       onClose={onClose}
-      anchorOrigin={{ vertical: "top", horizontal: "center" }} // Center the alert
+      anchorOrigin={{ vertical: "top", horizontal: "center" }}
     >
       <MuiAlert
         onClose={onClose}
         severity={type}
         sx={{
           width: "100%",
-          maxWidth: "600px", // Set a max width for larger alerts
-          fontSize: "1rem", // Increase font size
+          maxWidth: "600px",
+          fontSize: "1rem",
           marginTop: "15px",
-          paddingRight: "155px", // Add more padding
-          paddingLeft: "155px", // Add more padding
-          backgroundColor: backgroundColors[type] || "rgba(255, 255, 255, 0.9)", // Dynamic background color
+          paddingRight: "155px",
+          paddingLeft: "155px",
+          backgroundColor: backgroundColors[type] || "rgba(255, 255, 255, 0.9)",
           "& .MuiAlert-icon": {
-            fontSize: "1.5rem", // Increase the size of the icon
+            fontSize: "1.5rem",
           },
         }}
       >
